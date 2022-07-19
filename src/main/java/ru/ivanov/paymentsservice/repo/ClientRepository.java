3e0073872bbe7f287b0cbd101a1e31bbb6ac690e
Numiaa <pochtamaksa95@gmail.com>
2022-07-19 18:25:10 +0600

    n+1

--- src/main/java/ru/ivanov/paymentsservice/repo/ClientRepository.java
+++ src/main/java/ru/ivanov/paymentsservice/repo/ClientRepository.java
@@ -1,11 +1,16 @@
 package ru.ivanov.paymentsservice.repo;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.ivanov.paymentsservice.model.Client;
 
 import java.util.UUID;
 
 @Repository
 public interface ClientRepository extends CrudRepository<Client, UUID> {
+    @Transactional(readOnly = true)
+    @EntityGraph(attributePaths = "debts")
+    Iterable<Client> findAll();
 }
